90f123a1c3fc14a5bf4937c3998ca26e63690520
vivienseguy <vivienseguy@gmail.com>
2019-05-13 00:14:34 +0900

    divers

--- vivien_scripts/coreml_export_models.py
+++ vivien_scripts/coreml_export_models.py
@@ -52,25 +52,82 @@ class EncoderForCoreMLExport(nn.Module):
 
         super(EncoderForCoreMLExport, self).__init__()
 
+        self.input_size = input_size
+        self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.bidirectional = bidirectional
-
         self.decomposed_model_list = decomposed_model_list
 
 
-    def init_hidden(self, batch_size):
-        hidden_state_zeros = torch.zeros(batch_size, self.rnn_hidden_size)
-        return hidden_state_zeros
+    def init_hidden_list(self, batch_size):
+        hidden_input_list = []
+        for _ in range(len(self.rnn_cell_list)):
+            hidden_state_zeros = torch.zeros(batch_size, self.hidden_size)
+            cell_state_zeros = torch.zeros(batch_size, self.hidden_size)
+            if self.gpu:
+                hidden_state_zeros = hidden_state_zeros.cuda()
+                cell_state_zeros = cell_state_zeros.cuda()
+            hidden_input_list.append((hidden_state_zeros, cell_state_zeros))
+        return hidden_input_list
 
 
-    def forward(self, x, hidden=None):
-        
-
-
+    def forward(self, x, hidden_input_list=None):
 
         seq_length, batch_size, input_size = x.shape
-        assert batch_size == 1
-        h, c = self.rnn_cell(x, hidden if hidden else self.init_hidden(batch_size))
+
+        hidden_input_list = hidden_input_list if hidden_input_list else self.init_hidden_list(batch_size)
+
+        input_sequence = list(x.split(1))
+        output_sequence = []
+        output_sequence_reverse = []
+
+        for layer_index in range(self.num_layers):
+
+            rnn_cell = self.decomposed_model_list[layer_index * (1 + self.bidirectional)]
+
+            for t in range(len(input_sequence)):
+                h_t, c_t = rnn_cell(input_sequence[t].view([1, -1]), hidden_input_list[layer_index * (1 + self.bidirectional)] if t == 0 else (h_t, c_t))
+                output_sequence.append(h_t)
+
+            if self.bidirectional:
+
+                rnn_cell = self.decomposed_model_list[layer_index * (1 + self.bidirectional) + 1]
+
+                for t in range(len(input_sequence)-1, -1, -1):
+                    h_t_reverse, c_t_reverse = rnn_cell(input_sequence[t].view([1, -1]), hidden_input_list[layer_index * (1 + self.bidirectional) + 1] if t == 0 else (h_t_reverse, c_t_reverse))
+                    output_sequence_reverse.append(h_t_reverse)
+
+                outputs_concat = []
+                for t in range(len(input_sequence)):
+                    outputs_concat.append(torch.cat([output_sequence[t], output_sequence_reverse[len(input_sequence)-1-t]], dim=1))
+
+                input_sequence = outputs_concat
+
+            else:
+
+                input_sequence = output_sequence
+
+        return torch.cat([h_t, h_t_reverse], dim=1), 0.
+
+
+
+
+
+
+
+
+
+
+
+
+        # for layer_index in range(self.num_layers):
+        #
+        #     rnn_cell = self.decomposed_model_list[layer_index * (1 + self.bidirectional)]
+        #     h, c = rnn_cell(x, hidden if hidden else self.init_hidden(batch_size))
+
+
+
+
         return h, c
 
 
@@ -85,8 +142,9 @@ class SingleLayerLSTMForCoreMLExport(nn.Module):
         self.rnn_cell = nn.LSTMCell(input_size=input_size, hidden_size=hidden_size)
 
     def init_hidden(self, batch_size):
-        hidden_state_zeros = torch.zeros(batch_size, self.rnn_hidden_size)
-        return hidden_state_zeros
+        h = torch.zeros(batch_size, self.rnn_hidden_size)
+        c = torch.zeros(batch_size, self.rnn_hidden_size)
+        return (h, c)
 
     def forward(self, x, hidden=None):
         batch_size, input_size = x.shape
--- vivien_scripts/pytorch_to_onnx.py
+++ vivien_scripts/pytorch_to_onnx.py
@@ -10,12 +10,49 @@ from onmt.utils.misc import split_corpus
 from onmt.translate.translator import build_translator
 import onmt.opts as opts
 from onmt.utils.parse import ArgumentParser
-from vivien_scripts.coreml_export_models import decompose_bnn_lstm
+from vivien_scripts.coreml_export_models import decompose_bnn_lstm, EncoderForCoreMLExport
 
 
 dataset_root_path = '/Volumes/Extreme SSD/JP_EN_Translation_Data/pairwise_sentences'
 dataset_files = [file for file in os.listdir(dataset_root_path) if not file.startswith('.')]
-model_file_path = '/Volumes/Extreme SSD/OpenNMTModels/demo_jp-en_20k-model_step_800000.pt'
+
+model_file_folder = '/Volumes/Extreme SSD/OpenNMTModels'
+model_file_name = 'demo_jp-en_20k-model_step_800000.pt'
+model_file_path = os.path.join(model_file_folder, model_file_name)
+
+
+
+def test_rnn_and_coreml_models_equality(rnn_model, coreml_model):
+
+    input_size = rnn_model.input_size
+    hidden_size = rnn_model.hidden_size
+    num_layers = rnn_model.num_layers
+    bidirectional = rnn_model.bidirectional
+
+    input = torch.randn(1, 1, input_size)
+
+    hidden_state = torch.randn(num_layers * (1 + bidirectional), 1, hidden_size)
+    cell_state = torch.randn(num_layers * (1 + bidirectional), 1, hidden_size)
+
+    hidden_list = [(hidden_state[0,:,:], cell_state[0,:,:]), (hidden_state[1,:,:], cell_state[1,:,:]), (hidden_state[2,:,:], cell_state[2,:,:]), (hidden_state[3,:,:], cell_state[3,:,:])]
+
+    rnn_model.eval()
+    coreml_model.eval()
+
+    rnn_model_output, rnn_model_hidden = rnn_model(input, (hidden_state, cell_state))
+    coreml_model_output, coreml_model_hidden = coreml_model(input, hidden_list)
+
+    rnn_model_output = rnn_model_output.squeeze()
+    coreml_model_output = coreml_model_output.squeeze()
+
+    relative_diff = torch.sqrt(torch.sum((rnn_model_output - coreml_model_output)**2)).detach() / \
+                    torch.sqrt(torch.sum(coreml_model_output**2)).detach()
+
+    print('RNN / CoreML models relative output diff = {}'.format(relative_diff))
+
+    assert relative_diff < 0.001
+
+
 
 
 def pytorch_to_onnx(opt):
@@ -33,10 +70,36 @@ def pytorch_to_onnx(opt):
     model = onmt.model_builder.load_test_model(opt)
     print(model)
 
+    embeddings = model[1].encoder.embeddings
+    embeddings_input = torch.zeros((1, 1)).long()
+    input_names = ['input_index']
+    output_names = ['embedding']
+    torch.onnx.export(embeddings, embeddings_input,
+                      os.path.join(model_file_folder, 'embeddings.onnx'),
+                      verbose=True, input_names=input_names, output_names=output_names)
+
+
     encoder = model[1].encoder.rnn
     encoder_models = decompose_bnn_lstm(encoder)
+    coreml_encoder = EncoderForCoreMLExport(encoder.input_size, encoder.hidden_size, decomposed_model_list=encoder_models, num_layers=encoder.num_layers, bidirectional=encoder.bidirectional)
+    test_rnn_and_coreml_models_equality(encoder, coreml_encoder)
+    num_directions = 1 + encoder.bidirectional
+    for layer_index in range(encoder.number_layer):
+        for direction in range(num_directions):
+            encoder_model_part = encoder_models[layer_index * num_directions + direction]
+            encoder_input = (torch.randn(1, 1, encoder.input_size if layer_index == 0 else encoder.hidden_size * num_directions),
+                             encoder_model_part.init_hidden())
+            input_names = ['actual_input_1', 'hidden_input_1', 'cell_intput_1']
+            output_names = ['h', 'c']
+            torch.onnx.export(encoder_model_part, encoder_input,
+                              os.path.join(model_file_folder, 'encoder_model_{}.onnx'.format(layer_index * num_directions + direction)),
+                              verbose=True, input_names=input_names, output_names=output_names)
 
-    dummy_input = torch.randn(1, 1, 10, 10)
+    decoder = model[1].decoder
+
+
+
+
 
     # torch.onnx.export(model, dummy_input, os.path.join('../training/checkpoints', checkpoint_folder, 'model.onnx'),
     #                   verbose=True, input_names=input_names, output_names=output_names)
@@ -54,3 +117,8 @@ if __name__ == "__main__":
     parser = _get_parser()
     opt = parser.parse_args()
     pytorch_to_onnx(opt)
+
+
+
+
+
